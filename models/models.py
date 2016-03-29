7d41b681a2643982a16da7aa4a9e52c579a5f7ad
remixazka <remixazka@192.168.0.12>
2016-03-29 03:47:02 +0200

    Modificamos models.py para que instructor tenga un dominio, y que se muestren los que son instructores añadimos las categorías a la clase partner.py y a su xml.

--- models/models.py
+++ models/models.py
@@ -36,6 +36,8 @@ from openerp import models, fields, api
 #     duration = fields.Float(digits=(6, 2), help="Duration in days")
 #     seats = fields.Integer(string="Number of seats")
 
-#     instructor_id = fields.Many2one('res.partner', string="Instructor")
-#         course_id = fields.Many2one('openacademy.course',
+#    instructor_id = fields.Many2one('res.partner', string="Instructor",
+#         domain=['|', ('instructor', '=', True),
+                 ('category_id.name', 'ilike', "Teacher")]
+#    course_id = fields.Many2one('openacademy.course',
 #         ondelete='cascade', string="Course", required=True)
